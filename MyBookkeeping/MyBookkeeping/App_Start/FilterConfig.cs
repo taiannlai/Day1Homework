022d6ddd6d12b0c894434fcfcc15eb6b4927a948
taiannlai <30769612+taiannlai@users.noreply.github.com>
2017-08-06 17:31:42 +0800

    Initial commit

---
+++ MyBookkeeping/MyBookkeeping/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MyBookkeeping
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
